a66fcd5d5d4bb7a604eefd6d8fdc488f8e32b11f
Jijie Chen <jijie.chen@someplace.com>
2017-04-17 13:42:56 +0800

    Added build scripts

---
+++ Build.bat
@@ -0,0 +1,56 @@
+@setlocal 
+@set local=%~dp0
+@ECHO off
+
+@set CtxErrCode=0
+
+@REM Get path to MSBuild Binaries
+if exist "%windir%\Microsoft.NET\Framework64\v4.0.30319" SET MSBUILDEXEDIR=%windir%\Microsoft.NET\Framework64\v4.0.30319
+if exist "%windir%\Microsoft.NET\Framework\v4.0.30319" SET MSBUILDEXEDIR=%windir%\Microsoft.NET\Framework\v4.0.30319
+if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin" SET MSBUILDEXEDIR=%ProgramFiles(x86)%\MSBuild\14.0\bin
+if exist "%ProgramFiles%\MSBuild\14.0\bin" SET MSBUILDEXEDIR=%ProgramFiles%\MSBuild\14.0\bin
+if exist "%ProgramFiles(x86)%\MSBuild\15.0\bin" SET MSBUILDEXEDIR=%ProgramFiles(x86)%\MSBuild\15.0\bin
+if exist "%ProgramFiles%\MSBuild\15.0\bin" SET MSBUILDEXEDIR=%ProgramFiles%\MSBuild\15.0\bin
+
+
+@REM Can't multi-block if statement when check condition contains '(' and ')' char, so do as single line checks
+if NOT "%MSBUILDEXEDIR%" == "" SET MSBUILDEXE=%MSBUILDEXEDIR%\MSBuild.exe
+if "%MSBUILDEXEDIR%" == "" GOTO :MsBuildNotFound
+if NOT "%MSBUILDEXEDIR%" == "" GOTO :MsBuildFound
+
+
+:MsBuildFound
+@ECHO MsBuild Location = %MSBUILDEXE%
+@goto build
+
+
+:build
+"%MSBUILDEXE%" "%local%MyBridge.sln" /t:Rebuild /P:Configuration=Release
+IF ERRORLEVEL 1 GOTO :end
+@goto copy
+
+
+
+:copy
+robocopy "%local%src\MyBridge\bin\Release" "%local%release" /e
+@goto pack
+
+
+
+
+:pack
+@rem %local%\.nuget\NuGet pack %local%\src\MyProj\MyProj.nuspec  -OutputDirectory %local%\release
+@goto end
+
+
+
+:MsBuildNotFound
+@echo Could not found msbuild
+@SET CtxErrCode=2
+@goto end
+
+
+:end
+@pushd %local%
+IF NOT "%CtxErrCode%" == "0" EXIT /B %CtxErrCode%
+EXIT /B %ERRORLEVEL%
