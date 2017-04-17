3fedccbcb2e945cecd40470719b654e21b017e92
Jijie Chen <jijie.chen@someplace.com>
2017-04-17 13:55:37 +0800

    Added restore package build step; fixed build error due to referenced to missing pdb file

--- Build.bat
+++ Build.bat
@@ -11,6 +11,8 @@ if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin" SET MSBUILDEXEDIR=%ProgramFiles(
 if exist "%ProgramFiles%\MSBuild\14.0\bin" SET MSBUILDEXEDIR=%ProgramFiles%\MSBuild\14.0\bin
 if exist "%ProgramFiles(x86)%\MSBuild\15.0\bin" SET MSBUILDEXEDIR=%ProgramFiles(x86)%\MSBuild\15.0\bin
 if exist "%ProgramFiles%\MSBuild\15.0\bin" SET MSBUILDEXEDIR=%ProgramFiles%\MSBuild\15.0\bin
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin" SET MSBUILDEXEDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin
+
 
 
 @REM Can't multi-block if statement when check condition contains '(' and ')' char, so do as single line checks
@@ -25,6 +27,9 @@ if NOT "%MSBUILDEXEDIR%" == "" GOTO :MsBuildFound
 
 
 :build
+@ECHO Installing packages...
+"%local%.nuget\NuGet.exe" restore "%local%MyBridge.sln"
+@ECHO Building...
 "%MSBUILDEXE%" "%local%MyBridge.sln" /t:Rebuild /P:Configuration=Release
 IF ERRORLEVEL 1 GOTO :end
 @goto copy
