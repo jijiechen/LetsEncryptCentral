b1fc7eac06ec6e74f2d023c78e68df5873fc0f6f
Jijie Chen <jijie.chen@someplace.com>
2017-08-06 22:36:10 +0800

    Support semicolons (use ;;) and equal sign (normaly) in DNS configuration values

--- src/LetsEncryptCentral/Miscellaneous/KVConfigurationParser.cs
+++ src/LetsEncryptCentral/Miscellaneous/KVConfigurationParser.cs
@@ -8,10 +8,13 @@ namespace LetsEncryptCentral
     {
         public static Dictionary<string, string> Parse(string configuration, string[] requiredConfKeys = null)
         {
+            var encoderReplacement = string.Format("${0}$", Guid.NewGuid().ToString("N").Substring(28));
+
             var conf = configuration
+                        .Replace(";;", encoderReplacement)
                         .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
-                        .Select(part => part.Split('='))
-                        .Select(parts => new KeyValuePair<string, string>(parts[0], parts[1]))
+                        .Select(part => part.Replace(encoderReplacement, ";").Split('='))
+                        .Select(parts => new KeyValuePair<string, string>(parts[0], string.Join("=", parts.Skip(1)) /* Values can contain '=' */ ))
                         .Aggregate(new Dictionary<string, string>(), (dic, item) => { dic[item.Key] = item.Value; return dic; });
 
             if (requiredConfKeys != null)
