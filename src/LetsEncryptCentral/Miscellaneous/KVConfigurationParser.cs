24e2997db9c9f225ec5dd2e68fb60a5d3a4b60ea
Jijie Chen <jijie.chen@someplace.com>
2017-08-07 10:50:17 +0800

    Revert "Use repeats to encode a semicolon (use ;;) and an equal sign (use ==)": No need to encode equal sign, already supported in last commit
    This reverts commit 4006756c2c0927eb85f6cf28dfc3026d9c4bdd87.

--- src/LetsEncryptCentral/Miscellaneous/KVConfigurationParser.cs
+++ src/LetsEncryptCentral/Miscellaneous/KVConfigurationParser.cs
@@ -8,16 +8,13 @@ namespace LetsEncryptCentral
     {
         public static Dictionary<string, string> Parse(string configuration, string[] requiredConfKeys = null)
         {
-            var semicolonReplacement = string.Format("${0}$", Guid.NewGuid().ToString("N").Substring(28));
-            var equalSignReplacement = string.Format("${0}$", Guid.NewGuid().ToString("N").Substring(28));
+            var encoderReplacement = string.Format("${0}$", Guid.NewGuid().ToString("N").Substring(28));
 
             var conf = configuration
-                        .Replace(";;", semicolonReplacement)
-                        .Replace("==", equalSignReplacement)
+                        .Replace(";;", encoderReplacement)
                         .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
-                        .Select(part => part.Replace(semicolonReplacement, ";").Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries))
-                        .Where(parts => parts.Length > 1)
-                        .Select(parts => new KeyValuePair<string, string>(parts[0], parts[1].Replace(equalSignReplacement, "=")))
+                        .Select(part => part.Replace(encoderReplacement, ";").Split('='))
+                        .Select(parts => new KeyValuePair<string, string>(parts[0], string.Join("=", parts.Skip(1)) /* Values can contain '=' */ ))
                         .Aggregate(new Dictionary<string, string>(), (dic, item) => { dic[item.Key] = item.Value; return dic; });
 
             if (requiredConfKeys != null)
