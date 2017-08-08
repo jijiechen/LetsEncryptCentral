e23abc2da1a8b4d22975597d032e56602367f2b8
Jijie Chen <jijie.chen@someplace.com>
2017-08-08 13:31:44 +0800

    Try it's best to output meaningful error messages on unhandled exceptions instead of generic and useless messages.

--- src/LetsEncryptCentral/Program.cs
+++ src/LetsEncryptCentral/Program.cs
@@ -2,7 +2,9 @@
 using LetsEncryptCentral.Commands;
 using Microsoft.Extensions.CommandLineUtils;
 using System;
+using System.Net;
 using System.Reflection;
+using ACMESharp;
 using static LetsEncryptCentral.ConsoleUtils;
 
 namespace LetsEncryptCentral
@@ -21,9 +23,11 @@ namespace LetsEncryptCentral
             System.Diagnostics.Debugger.Launch();
 #endif
 
-            var app = new CommandLineApplication();
-            app.Name = ApplicationName;
-            app.FullName = "A central Let's Encrypt client that applies certificates using the DNS-01 challenge";
+            var app = new CommandLineApplication
+            {
+                Name = ApplicationName,
+                FullName = "A central Let's Encrypt client that applies certificates using the DNS-01 challenge"
+            };
 
             app.HelpOption("-?|-h|--help");
             app.VersionOption("--version", Assembly.GetExecutingAssembly().GetName().Version.ToString());
@@ -40,20 +44,47 @@ namespace LetsEncryptCentral
 
             app.Execute(args);
         }
-
         
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
+            Console.WriteLine();
             ConsoleErrorOutput("!!! Unhandled exception has occured,  application is now exiting. !!!");
             var exception = e.ExceptionObject as Exception;
-            if (exception != null)
+            if (exception == null)
+            {
+                Environment.Exit(999);
+                return;
+            }
+            
+
+            var exceptionPrinted = false;
+            var acmeWebException = exception as AcmeClient.AcmeWebException;
+            var webException = exception as WebException;
+            if (acmeWebException != null)
+            {
+                if (!string.IsNullOrEmpty(acmeWebException.Response?.ContentAsString))
+                {
+                    exceptionPrinted = true;
+                    ConsoleErrorOutput(acmeWebException.Response.ContentAsString);
+                }
+                else if (acmeWebException.WebException != null)
+                {
+                    exceptionPrinted = true;
+                    ConsoleErrorOutput($"{acmeWebException.WebException.Status}: {acmeWebException.WebException.Message}");
+                }
+            }
+            else if (webException != null)
+            {
+                exceptionPrinted = true;
+                ConsoleErrorOutput($"{webException.Status}: {webException.Message}");
+            }
+
+            if (!exceptionPrinted)
             {
                 ConsoleErrorOutput(exception.Message);
                 ConsoleErrorOutput(exception.StackTrace);
             }
-
-            Environment.Exit(999);
-       }
-        
+            Environment.Exit(1);
+        }
     }
 }
